477110e67ce41cba5aa5ec547b749627d362ebac
Eugene Obrezkov <ghaiklor@gmail.com>
2019-01-08 15:27:16 +0200

    Implement smoke tests for adapter

--- test/helpers/createEnforcer.js
+++ test/helpers/createEnforcer.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const { newEnforcer } = require('casbin');
-const Adapter = require('../..');
+const MongooseAdapter = require('../..');
 
 module.exports = async function createEnforcer () {
   const model = path.resolve(__dirname, '../fixtures/basic_model.conf');
-  const adapter = await Adapter.newAdapter('mongodb://localhost:27017/casbin');
+  const adapter = await MongooseAdapter.newAdapter('mongodb://localhost:27017/casbin');
 
   return newEnforcer(model, adapter);
 };
--- test/integration/adapter.test.js
+++ test/integration/adapter.test.js
@@ -1,7 +1,49 @@
+const { assert } = require('chai');
 const createEnforcer = require('../helpers/createEnforcer');
+const CasbinRule = require('../../src/model');
 
-describe('Casbin', () => {
-  it('Should properly instantiate adapter and connect to mongo', async () => {
+// These tests are just smoke tests for get/set policy rules
+// We do not need to cover other aspects of casbin, since casbin itself is covered with tests
+describe('MongooseAdapter', () => {
+  beforeEach(async () => {
+    await createEnforcer();
+    await CasbinRule.deleteMany();
+  });
+
+  it('Should properly load policy', async () => {
     const enforcer = await createEnforcer();
+    assert.deepEqual(await enforcer.getPolicy(), []);
+
+    const rules = await CasbinRule.find();
+    assert.deepEqual(rules, []);
+  });
+
+  it('Should properly store new policy rules', async () => {
+    const enforcer = await createEnforcer();
+
+    const rulesBefore = await CasbinRule.find();
+    assert.deepEqual(rulesBefore, []);
+    assert.isTrue(await enforcer.addPolicy('sub', 'obj', 'act'));
+    assert.deepEqual(await enforcer.getPolicy(), [['sub', 'obj', 'act']]);
+
+    const rulesAfter = await CasbinRule.find({ p_type: 'p', v0: 'sub', v1: 'obj', v2: 'act' });
+    assert.equal(rulesAfter.length, 1);
+  });
+
+  it('Should properly delete existing policy rules', async () => {
+    const enforcer = await createEnforcer();
+
+    const rulesBefore = await CasbinRule.find();
+    assert.deepEqual(rulesBefore, []);
+    assert.isTrue(await enforcer.addPolicy('sub', 'obj', 'act'));
+    assert.deepEqual(await enforcer.getPolicy(), [['sub', 'obj', 'act']]);
+
+    const rulesAfter = await CasbinRule.find({ p_type: 'p', v0: 'sub', v1: 'obj', v2: 'act' });
+    assert.equal(rulesAfter.length, 1);
+    assert.isTrue(await enforcer.removePolicy('sub', 'obj', 'act'));
+    assert.deepEqual(await enforcer.getPolicy(), []);
+
+    const rulesAfterDelete = await CasbinRule.find({ p_type: 'p', v0: 'sub', v1: 'obj', v2: 'act' });
+    assert.equal(rulesAfterDelete.length, 0);
   });
 });
