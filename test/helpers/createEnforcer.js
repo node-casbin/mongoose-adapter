ea20ab313a2ffd3d64eaeed3a6a7c24583f56eb8
Eugene Obrezkov <ghaiklor@gmail.com>
2019-01-09 12:44:19 +0200

    Fix deprecations in mongoose

--- test/helpers/createEnforcer.js
+++ test/helpers/createEnforcer.js
@@ -2,9 +2,11 @@ const path = require('path');
 const { newEnforcer } = require('casbin');
 const MongooseAdapter = require('../..');
 
+const MONGOOSE_OPTIONS = { useNewUrlParser: true, useCreateIndex: true };
+
 module.exports = async function createEnforcer () {
   const model = path.resolve(__dirname, '../fixtures/basic_model.conf');
-  const adapter = await MongooseAdapter.newAdapter('mongodb://localhost:27017/casbin');
+  const adapter = await MongooseAdapter.newAdapter('mongodb://localhost:27017/casbin', MONGOOSE_OPTIONS);
 
   return newEnforcer(model, adapter);
 };
