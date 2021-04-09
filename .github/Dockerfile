27243f4a5843c78a3fb2135c3ea09ece7e17b492
Zxilly <zhouxinyu1001@gmail.com>
2021-04-09 16:18:08 +0800

    ci: add mongodb setup files

---
+++ .github/Dockerfile
@@ -0,0 +1,9 @@
+FROM mongo:latest
+
+VOLUME /data
+EXPOSE 27001 27002 27003
+
+COPY docker-entrypoint.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+
+ENTRYPOINT ["docker-entrypoint.sh"]
--- .github/mongo_setup.sh
+++ .github/mongo_setup.sh
@@ -1,15 +1,3 @@
-docker run -d -p 27001:27001 -p 27002:27002 -p 27003:27003 --name mongo -e "REPLICA_SET_NAME=rs0" zxilly/mongodb-rs
+docker build -t mongodb-rs:latest ./.github/
 
-echo "Waiting for MongoDB to accept connections"
-TIMER=0
-until docker exec --tty mongo mongo --port 27001 --eval "db.serverStatus()"
-do
-  sleep 1
-  echo "."
-  TIMER=$((TIMER + 1))
-
-  if [[ $TIMER -eq 10 ]]; then
-    echo "MongoDB did not initialize within 10 seconds. Exiting."
-    exit 2
-  fi
-done
+docker run -d -p 27001:27001 -p 27002:27002 -p 27003:27003 --name mongo -e "REPLICA_SET_NAME=rs0" mongodb-rs
