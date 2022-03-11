92b3aec67bd0e259ed63c2cb230bee462fa406c7
Zixuan Liu <nodeces@gmail.com>
2022-03-11 23:05:17 +0800

    revert: fix: Schema p_type to ptype (#56) (#58)
    This reverts commit 7a321669a06d8761ea8a20b93dfb98a53f0c355f.

--- src/model.ts
+++ src/model.ts
@@ -15,7 +15,7 @@
 import {Schema, Document, model} from 'mongoose';
 
 export interface IModel extends Document {
-  ptype: string;
+  p_type: string;
   v0: string;
   v1: string;
   v2: string;
@@ -25,7 +25,7 @@ export interface IModel extends Document {
 }
 
 const schema = new Schema({
-  ptype: {
+  p_type: {
     type: Schema.Types.String,
     required: true,
     index: true
